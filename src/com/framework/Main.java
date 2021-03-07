5c0b7601c89197c847f535638b3ce7be37853a11
Khaliqyar Haidary <khaliqyar18@gmail.com>
2021-03-07 10:42:37 -0500

    This is my first Java Dave Project

---
+++ src/com/framework/Main.java
@@ -0,0 +1,13 @@
+package com.framework;
+
+public class Main {
+    public static void main(String[] args) throws InterruptedException {
+        for(int i =0; i<6;i++){
+            System.out.println("Runing");
+
+            Thread.sleep(2000);
+
+        }
+    }
+
+}
